0b93fb42447caba8abb9a18c8cd2cbaa8ef1e3de
Luan Jardim <luang.ajardim@gmail.com>
2024-01-02 10:23:27 -0300

    Day 19: Second Half, Solved!

--- 2015-zig/day19/main.zig
+++ 2015-zig/day19/main.zig
@@ -8,13 +8,7 @@ const end = 43;
 pub fn main() !void {
 
     var map = Map.init(std.heap.page_allocator);
-    defer {
-        var it = map.iterator();
-        while(it.next()) |e| {
-            e.value_ptr.deinit();
-        }
-        map.deinit();
-    }
+    defer map.deinit();
 
     var cnt : usize = 1;
     var iter = mem.tokenizeAny(u8, file, " \n");
@@ -46,27 +40,23 @@ pub fn main() !void {
     var it = map.iterator();
     while(it.next()) |e| {
         const key = e.key_ptr.*;
-        // print("at key: {s}\n", .{key});
-        // print("\n", .{});
         for(e.value_ptr.*.items) |w| {
             for__ : for(0..entry.len) |i| {
-                const s = try std.fmt.allocPrint(alok, "{s}{s}{s}", .{entry[0..i], w, if(i+key.len-1 == entry.len) "" else entry[i+key.len..entry.len]});
                 if(entry[i] == key[0] and mem.eql(u8, entry[i..i+key.len], key)) {
+                    //create a new string with the substitution
+                    const s = try std.fmt.allocPrint(alok, "{s}{s}{s}", .{entry[0..i], w, if(i+key.len-1 == entry.len) "" else entry[i+key.len..entry.len]});
+                    //check if the string is already in the list
                     for(changes.items) |c| {
                         if(mem.eql(u8, c, s)) {
                             continue : for__;
                         }
                     }
+                    //if not, add it
                     try changes.append(s);
                 }
             }
         }
     }
-    // for(changes.items) |c| {
-    //     var buf = [_]u8{0} ** 1024;
-    //     const sla = try std.fmt.bufPrint(&buf, "{s}{s}{s}\n", .{entry[0..c.pos], c.str, entry[c.pos+c.prevLen..entry.len]});
-    //     try std.io.getStdOut().writer().print("{s}", .{sla});
-    // }
     print("changes: {d}\n", .{changes.items.len});
 
     //print the whole map
@@ -78,6 +68,38 @@ pub fn main() !void {
 //         }
 //         print("}}\n", .{});
 //     }
+
+    //to solve the second half we must analyze the entry to see some points about the grammar pattern
+    //
+    // All it's productions have the from : X -> XX | X Rn X (YX)* Ar
+    // where, X is a variable of the grammar, and Rn, Y and Ar are terminals
+    // so, to find the number of steps to reach the molecule we can use these patterns
+    //
+    // and the formula: steps = qtdElements - count(Rn) - 2*count(Y) - count(Ar) - 1
+    // that comes from the fact that:
+    // X = XX -> X, steps = qtdElements -0 -0 -0 -1 = 1
+    // X = XRnXAr -> X,steps = qtdElements -1 -0 -1 -1 = 1
+    // X = XRnXYXAr -> X, steps = qtdElements -1 -2 -1 -1 = 1
+
+    var count : usize = 0;
+    var countSpecial : usize = 0;
+    //find the quantity of elements the entry has, and count Rn, Y and Ar
+    for(0..entry.len) |i| {
+        if(map.contains(entry[i..i+1]) or (i+1 < entry.len and map.contains(entry[i..i+2]))) {
+            count += 1;
+        } else {
+            if(i+1 < entry.len and (mem.eql(u8, entry[i..i+2], "Rn") or mem.eql(u8, entry[i..i+2], "Ar"))) {
+                count += 1;
+                countSpecial += 1;
+            }
+            if(mem.eql(u8, entry[i..i+1], "Y")) {
+                count += 1;
+                countSpecial += 2;
+            }
+        }
+    }
+    const steps = count - countSpecial - 1;
+    print("steps: {d}\n", .{steps});
 }
 
 test "alloc n free test" {
